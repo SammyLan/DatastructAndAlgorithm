fc1e68c5b5d81bb50c8f48442ad1f602562d546d
SammyLan <sammylan234@gmail.com>
2018-11-10 22:40:12 +0800

    base version

---
+++ LeetCode/LeetCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode
+//
+//  Created by 蓝秀文 on 2018/11/10.
+//  Copyright © 2018 蓝秀文. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
